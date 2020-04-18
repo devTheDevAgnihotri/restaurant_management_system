69f3b5520821d0bff9f143de35ccce5980e6b261
devagni98 <devagnihotrid@gmail.com>
2020-04-18 18:24:53 +0530

    Oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId:'590286341835210',
+        clientSecret:'c5f003be4a67a9d8d8757e3839bbc1d6',
+    }
 }
